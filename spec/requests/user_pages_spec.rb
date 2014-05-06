e41413090bc3265ad28175b14c0e646727ff657c
Evgenia Kiseleva <janekis@aol.com>
2014-05-06 00:27:30 -0500

    update to the chapter 9

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -89,18 +89,18 @@ describe "User pages" do
 
     describe "with valid information" do
       before { valid_information() }
-
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_success_message('Welcome') }
-      end
-      
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
